f7b66c6ca486ab3246e3e79e3c2cfe5b96590487
claricealvs <claricealves330@gmail.com>
2024-08-06 17:11:28 -0300

    ✨ feat: Adiciona conn.sync()

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 
 const conn = require('./bd/conn.js')
+const Tutor = require('./models/Tutor.js')
+const Tutor = require('./models/Pet.js')
 
 
-app.listen(3000)
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
