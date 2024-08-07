448d68484b65f2a6b9907c1cba1a38976fffbffd
claricealvs <claricealves330@gmail.com>
2024-08-07 19:37:26 -0300

    ♻️ refactor: Reaftora método get

--- src/api/routes/tutors.js
+++ src/api/routes/tutors.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Tutor = require("../models/Tutor.js");
 
-router.get("/tutors", (req, res) => {});
+router.get("/tutors", async (req, res) => {
+  const tutors = await Tutor.findAll({ raw: true });
+  console.log("Tutores", tutors);
+  res.status(201).json(tutors);
+});
 
+// adiciona tutor
 router.post("/tutor", (req, res) => {
   const { name, phone, email, date_of_birth, zip_code } = req.body;
-
   console.log(req.body);
 
   Tutor.create({
@@ -17,6 +21,34 @@ router.post("/tutor", (req, res) => {
     zip_code,
   })
     .then(() => res.send("Tutor criado"))
+    .catch((err) => console.log(err));
+});
+
+// atualiza tutor
+router.post("/tutor/:id", (req, res) => {
+  const { name, phone, email, date_of_birth, zip_code } = req.body;
+  const id = req.params.id;
+
+  // cria objeto com todas as propriedades de user
+  const tutorData = {
+    id,
+    name,
+    phone,
+    email,
+    date_of_birth,
+    zip_code,
+  };
+
+  console.log(tutorData);
+
+  Tutor.update(tutorData, {
+    where: {
+      id: id,
+    },
+  })
+    .then((tutor) => {
+      res.send("Tutor atualizado");
+    })
     .catch((err) => console.log(err));
 });
 
